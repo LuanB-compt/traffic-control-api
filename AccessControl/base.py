36ba42514f9d3c52cd9cc3270811651d0707576f
PiresFelipe <felipepires725@gmail.com>
2023-05-22 19:36:13 -0300

    Updated [base]: Added CORS policy

--- AccessControl/base.py
+++ AccessControl/base.py
@@ -1,17 +1,20 @@
 import flask as fl
 import flask_cors
-from .routes import car, person, predict
+from .routes import car, monitoring, person
 from .data.database import db
 from .config import Config
 
 def create_app():
     app = fl.Flask(__name__)
+    cors = flask_cors.CORS(app)
 
     app.config.from_object(obj=Config)
     app.register_blueprint(blueprint=car.bp)
     app.register_blueprint(blueprint=person.bp)
+    app.register_blueprint(blueprint=monitoring.bp)
     
     db.init_app(app=app)
+    
     with app.app_context():
         db.create_all()
         flask_cors.CORS(app=app)
